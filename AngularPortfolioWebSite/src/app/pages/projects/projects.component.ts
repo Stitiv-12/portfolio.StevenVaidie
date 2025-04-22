d98cbda871756cbb76e2c30c06d6c804eccc19c7
dispro25 <dispro25.cimpa@gmail.com>
2025-04-22 10:29:57 +0200

    link github

--- AngularPortfolioWebSite/src/app/pages/projects/projects.component.ts
+++ AngularPortfolioWebSite/src/app/pages/projects/projects.component.ts
@@ -24,7 +24,7 @@ export class ProjectsComponent implements OnInit {
       title: 'Sanatio App-Mobile',
       fragment: 'sanatio-app-mobile',
       objective: "Création d'une application mobile pour aider les patients à gérer leurs traitements médicaux.",
-      realisation: "Ce projet est en cours avec 6 autres personnes, en utilisant React-Native pour le front-end et Express pour les API.",
+      realisation: "J'ai réalisé ce projet avec 6 autres personnes, en utilisant React-Native pour le front-end et Express pour les API.",
       description: "L'application permet aux utilisateurs de gérer leurs prescriptions, suivre leur calendrier de médicaments et consulter les stocks disponibles.",
       functions: [
         'Ajout de prescriptions manuelles ou via un scan d\'ordonnance',
@@ -34,9 +34,49 @@ export class ProjectsComponent implements OnInit {
       githubUrl: 'https://github.com/Stitiv-12/Sanatio-App',
     },
     {
+      title: 'Générateur de Particules',
+      fragment: 'generateur-de-particules',
+      objective: 'Apprendre Go en créant un générateur de particules interactif à partir d\'une images correspondant à une particule.',
+      realisation: 'Création d\'un logiciel permettant de générer des particules et de les manipuler à l\'aide de la souris.',
+      description: 'Ce projet a exploré les bases de la programmation en Go tout en appliquant des concepts d\'animations et de gestion de particules.',
+      functions: [
+        'Génération de particules à partir d\'un pixel',
+        'Modification de la couleur et taille des particules',
+        'Déplacement et création de particules en fonction du clic ou du mouvement de la souris',
+        'Simulation de gravité et collisions entre particules'
+      ],
+      githubUrl: 'https://github.com/Stitiv-12/Generateur_particule'
+    },
+    {
+      title: 'Puissance 4',
+      fragment: 'puissance-4',
+      objective: 'Réaliser un jeu Puissance 4 avec gestion de serveur pour l\'interaction entre joueurs.',
+      realisation: 'Développement du jeu en Go, avec gestion des Goroutines pour la communication entre les joueurs via un serveur.',
+      description: 'Ce projet a permis de découvrir la gestion des connexions et de la communication en temps réel entre deux utilisateurs.',
+      functions: [
+        'Jeu Puissance 4 multijoueur en ligne',
+        'Gestion des interactions entre deux joueurs via un serveur',
+        'Utilisation de Goroutines pour gérer la communication asynchrone'
+      ],
+      githubUrl: 'https://github.com/Stitiv-12/Puissance-4'
+    },
+    {
+      title: 'Pickomino',
+      fragment: 'pickomino',
+      objective: 'Réaliser le jeu de société Pickomino en version numérique.',
+      realisation: 'Projet en équipe de 4 personnes, développement avec Kotlin et JavaFX pour la partie graphique et logique du jeu.',
+      description: 'Une adaptation numérique du jeu de société où les règles ont été reproduites fidèlement pour offrir une expérience de jeu fluide.',
+      functions: [
+        'Interface graphique avec JavaFX',
+        'Gestion des règles du jeu de société Pickomino',
+        'Multijoueur et interactions entre joueurs'
+      ],
+      githubUrl: 'https://github.com/Stitiv-12/Pickomino'
+    },
+    {
       title: 'Site e-commerce de parfum',
       fragment: 'site-e-commerce-de-parfum',
-      objective: 'Mettre en œuvre les compétences en Laravel et PHP sur un projet concret de site e-commerce.',
+      objective: 'Mettre en œuvre mes compétences en Laravel et PHP sur un projet concret de site e-commerce.',
       realisation: 'Projet réalisé avec 3 autres personnes. Il s\'agit d\'un site complet avec gestion des produits, paiement et profils utilisateurs.',
       description: 'Les utilisateurs peuvent acheter des parfums, gérer leur panier, et passer commande via un processus de paiement sécurisé.',
       functions: [
@@ -59,46 +99,6 @@ export class ProjectsComponent implements OnInit {
         'Stockage et gestion des fichiers AML sur Google Drive'
       ],
       githubUrl: 'https://github.com/Stitiv-12/aml-converter'
-    },
-    {
-      title: 'Générateur de Particules',
-      fragment: 'generateur-de-particules',
-      objective: 'Apprendre Go en créant un générateur de particules interactif à partir d\'images.',
-      realisation: 'Création d\'un logiciel permettant de générer des particules et de les manipuler à l\'aide de la souris.',
-      description: 'Ce projet a exploré les bases de la programmation en Go tout en appliquant des concepts d\'animations et de gestion de particules.',
-      functions: [
-        'Génération de particules à partir d\'un pixel',
-        'Modification de la couleur et taille des particules',
-        'Déplacement et création de particules en fonction du clic ou du mouvement de la souris',
-        'Simulation de gravité et collisions entre particules'
-      ],
-      githubUrl: 'https://github.com/Stitiv-12/particle-generator'
-    },
-    {
-      title: 'Puissance 4',
-      fragment: 'puissance-4',
-      objective: 'Réaliser un jeu Puissance 4 avec gestion de serveur pour l\'interaction entre joueurs.',
-      realisation: 'Développement du jeu en Go, avec gestion des Goroutines pour la communication entre les joueurs via un serveur.',
-      description: 'Ce projet a permis de découvrir la gestion des connexions et de la communication en temps réel entre deux utilisateurs.',
-      functions: [
-        'Jeu Puissance 4 multijoueur en ligne',
-        'Gestion des interactions entre deux joueurs via un serveur',
-        'Utilisation de Goroutines pour gérer la communication asynchrone'
-      ],
-      githubUrl: 'https://github.com/Stitiv-12/puissance4'
-    },
-    {
-      title: 'Pickomino',
-      fragment: 'pickomino',
-      objective: 'Réaliser le jeu de société Pickomino en version numérique.',
-      realisation: 'Projet en équipe de 4 personnes, développement avec Kotlin et JavaFX pour la partie graphique et logique du jeu.',
-      description: 'Une adaptation numérique du jeu de société où les règles ont été reproduites fidèlement pour offrir une expérience de jeu fluide.',
-      functions: [
-        'Interface graphique avec JavaFX',
-        'Gestion des règles du jeu de société Pickomino',
-        'Multijoueur et interactions entre joueurs'
-      ],
-      githubUrl: 'https://github.com/Stitiv-12/pickomino'
     }
   ];
   constructor(
